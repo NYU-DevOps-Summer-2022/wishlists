6caff315bca1d756e66e7dab31ed71638c6f88e7
neerajkanuri <neerajkanuri@gmail.com>
2022-06-29 07:03:38 +0530

    Add products to a wishlist (#23)
    * add products to a wishlist

--- tests/factories.py
+++ tests/factories.py
@@ -5,7 +5,7 @@ import factory
 import random
 from datetime import date
 from factory.fuzzy import FuzzyChoice, FuzzyDate
-from service.models import Wishlist
+from service.models import Item, Wishlist
 
 class WishlistFactory(factory.Factory):
     """Creates fake Wishlists"""
@@ -18,3 +18,15 @@ class WishlistFactory(factory.Factory):
     id = factory.Sequence(lambda n: n)
     name = factory.Faker("name")
     customer_id = factory.LazyAttribute(lambda x: random.randrange(0, 10000))
+
+class ItemFactory(factory.Factory):
+    """Creates fake Wishlist items"""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Maps factory to data model"""
+
+        model = Item
+    
+    id = factory.Sequence(lambda n: n)
+    wishlist_id = factory.LazyAttribute(lambda x: random.randrange(0, 10000))
+    product_id = factory.LazyAttribute(lambda x: random.randrange(0, 10000))
